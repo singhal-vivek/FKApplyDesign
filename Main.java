fa64ebe8ac5bad3330d917eb47216985f7636434
singhal-vivek <vivek23399@gmail.com>
2020-01-25 10:23:36 +0530

    Update Main.java
    define human class which implements player interface

--- Main.java
+++ Main.java
@@ -14,20 +14,43 @@ class Grid {
 				}
 			}
 		}
+}
+
+interface Player {
+
+	void fill(int a,int b,Grid grid);
+}
+
+class Human implements Player {
+	int id;
+	Human(int x)
+	{
+		this.id = x;
 	}
+	int validMove(int a,int b,Grid grid)
+	{
+		if(a>=0&&a<3&&b>=0&&b<3&&grid.board[a][b]==-1)
+		{
+			return 1;
+		}
+		return 0;
+	}
+	public void fill(int a,int b,Grid grid)
+	{
+		if(validMove(a,b,grid)==1)
+		{
+			grid.board[a][b] = id;
+		}
+	}
+}
 
 public class Main {
 
 	public static void main(String[] args) {
 		Grid grid = new Grid();
-		int i,j;
-		for(i=0;i<3;i++)
-		{
-			for(j=0;j<3;j++)
-			{
-				System.out.println(grid.board[i][j]+ " ");
-			}
-		}
+		Human h1 = new Human(1);
+		Human h2 = new Human(2);
+
 
 	}
 }
