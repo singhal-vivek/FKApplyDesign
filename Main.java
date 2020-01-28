cbecdf6b7cc41d433a2b0eb22b70ae960ac51fcf
singhal-vivek <vivek23399@gmail.com>
2020-01-28 14:25:02 +0530

    for any value of n

--- Main.java
+++ Main.java
@@ -1,77 +1,153 @@
-package Design;
 
 import java.util.Scanner; 
 class Grid {
-		int[][] board = new int[3][3];
+		
 		 int i,j;
-		Grid()
+		 int fwin=-1;
+		 int n;
+		 int[][] board;
+		 int[] box;
+		Grid(int tt)
 		{
-			
-			for(i=0;i<3;i++)
+			n = tt;
+			board = new int[n][n];
+			box = new int[n];
+			for(i=0;i<n;i++)
 			{
-				for(j=0;j<3;j++)
+				box[i] = -1;
+			}
+			for(i=0;i<n;i++)
+			{
+				for(j=0;j<n;j++)
 				{
-					this.board[i][j] = -1;
+					board[i][j] = -1;
 				}
 			}
 		}
 		public void showBoard() {
-			for(i=0;i<3;i++)
+			for(i=0;i<n;i++)
 			{
-				for(j=0;j<3;j++)
+				for(j=0;j<n;j++)
 				{
-					System.out.print(this.board[i][j] + "	");
+					System.out.print(board[i][j] + "   ");
+					if(j%3==2)
+						System.out.print("  ||   ");
 				}
+				
 				System.out.println("");
+				if(i%3 == 2)
+				{
+				    System.out.println("=====================================================================================================================================================");
+				}
+
 			}
 		}
-		int validMove(int a,int b)
+		int validMove(int a,int b,int k)
 		{
-			if(a>=0&&a<3&&b>=0&&b<3&&this.board[a][b]==-1)
+			if(a>=0&&a<n&&b>=0&&b<n&&board[a][b]==-1&&(a/3)*3+b/3==k)
 			{
 				return 1;
 			}
 			return 0;
 		}
-		boolean  row(int x,int y)
+		boolean  row(int x,int y,int rt)
 		{
-			int p = this.board[x][y];
-			for(i=0;i<3;i++)
+			int p = board[x][y];
+			for(i=3*(rt%3);i<3+3*(rt%3);i++)
 			{
-				if(this.board[x][i] != p)
+				if(board[x][i] != p)
 					return false;
 			}
 			return true;
 
 		}
-		boolean  coloumn(int x,int y)
+		boolean  coloumn(int x,int y,int rt)
 		{
-			int p = this.board[x][y];
-			for(i=0;i<3;i++)
+			int p = board[x][y];
+			for(i=3*(rt/3);i<3+3*(rt/3);i++)
 			{
-				if(this.board[i][y] != p)
+				if(board[i][y] != p)
 					return false;
 			}
 			return true;
 
 		}
-		boolean dig1(int p)
+		boolean dig1(int p,int rt)
 		{
-			if(this.board[0][0]==p && this.board[1][1]==p && this.board[2][2]==p)
+			if(board[3*(rt/3)+0][0+3*(rt%3)]==p && board[3*(rt/3)+1][1+3*(rt%3)]==p && board[3*(rt/3)+2][2+3*(rt%3)]==p)
 				return true;
 			return false;
 		}
-		boolean dig2(int p)
+		boolean dig2(int p,int rt)
 		{
-			if(this.board[0][2]==p && this.board[1][1]==p && this.board[2][0]==p)
+			if(board[0+3*(rt/3)][2+3*(rt%3)]==p && board[1+3*(rt/3)][1+3*(rt%3)]==p && board[2+3*(rt/3)][0+3*(rt%3)]==p)
 				return true;
 			return false;
 		}
 		boolean winner(int x,int y)
 		{
-			int p = this.board[x][y];
-			return row(x,y) || coloumn(x,y) || dig1(p) || dig2(p);
+			int p = board[x][y];
+			int rt = (x/3)*3 + y/3;
+			return row(x,y,rt) || coloumn(x,y,rt) || dig1(p,rt) || dig2(p,rt);
 			
+		}
+		boolean fwinner()
+		{
+			int fk=0;
+			for(i=0;i<n/3;i++)
+			{
+				fk=0;
+				for(j=0;j<n/3-1;j++)
+				{
+					if(box[i*(n/3)+j] != box[i*(n/3)+j+1] || box[i*(n/3)+j]==-1 ||  box[i*(n/3)+j+1]==-1)
+					{
+						fk=1;
+						break;
+					}
+				}
+				if(fk==0)
+					return true;
+			}
+			for(i=0;i<n/3;i++)
+			{
+				fk=0;
+				for(j=0;j<n/3-1;j++)
+				{
+					if(box[i+j*(n/3)]!=box[i+j*(n/3)+1]||box[i+j*(n/3)]==-1||box[i+j*(n/3)+1]==-1)
+					{
+						fk=1;
+						break;
+					}
+				}
+				if(fk==0)
+					return true;
+			}
+			for(i=0;i<n/3-1;i++)
+			{
+				fk=0;
+				if(box[i*(n/3+1)]!=box[(i+1)*(n/3+1)]||box[i*(n/3+1)]==-1||box[(i+1)*(n/3+1)]==-1)
+				{
+					fk=1;
+					break;
+				}
+				
+			}
+			if(fk==0)
+				return true;
+			j=n/3-1;
+			for(i=0;i<n/3-1;i++)
+			{
+				fk=0;
+				if(box[j]!=box[j+(n/3-1)]||box[j]==-1||box[j+(n/3-1)]==-1)
+				{
+					fk=1;
+					break;
+				}
+				j=j+(n/3)-1;
+			}
+			if(fk==0)
+				return true;
+			return false;
 		}
 }
 
@@ -98,62 +174,158 @@ class Human implements Player {
 public class Main {
 
 	public static void main(String[] args) {
+		int n;
+		System.out.println("enter the value of n::");
+		
 		Scanner get = new Scanner(System.in); 
-		Grid grid = new Grid();
+		n=get.nextInt();
+		Grid grid = new Grid(n);
 		Human h1 = new Human(0);
 		Human h2 = new Human(1);
 		grid.showBoard();
 		int flag = 0;
 		int count = 0;
-		while(count < 9)
+		while(count < n)
 		{
 			int x,y;
-			if(flag == 0)
+			int b;
+			if(flag==0)
 			{
-				System.out.println("Player_1 enter position to fill::");
-				x = get.nextInt();
-				y = get.nextInt();
-				if(grid.validMove(x,y)==1)
+				System.out.println("Player_0 enter box number to fill::");
+				b = get.nextInt();
+				if(grid.box[b] == -1)
 				{
-					h1.fill(x,y,grid);
-					flag = 1;
 					count++;
-					if(grid.winner(x,y))
+					int z = 0;
+					while(z<9)
 					{
-						System.out.println("Player_1 Wins");
-						grid.showBoard();
-						break;
+						if(flag == 0)
+						{
+							System.out.println("Player_0 enter position to fill::");
+							x = get.nextInt();
+							y = get.nextInt();
+							if(grid.validMove(x,y,b)==1)
+							{
+								h1.fill(x,y,grid);
+								grid.showBoard();
+								flag = 1;
+								z++;
+								if(grid.winner(x,y))
+								{
+									System.out.println("Player_0 Wins this block");
+									grid.box[b] = 0;
+									grid.showBoard();
+									break;
+								}
+							}
+							else
+							{
+								System.out.println("not a valid move");
+							}
+						}
+						else
+						{
+							System.out.println("Player_1 enter position to fill::");
+							x = get.nextInt();
+							y = get.nextInt();
+							if(grid.validMove(x,y,b)==1)
+							{
+								h2.fill(x,y,grid);
+								grid.showBoard();
+								flag = 0;
+								z++;
+								if(grid.winner(x,y))
+								{
+									System.out.println("Player_1 Wins this box");
+									grid.box[b] = 1;
+									grid.showBoard();
+									break;
+								}
+							}
+							else
+							{
+								System.out.println("not a valid move");
+							}
+						}
 					}
 				}
 				else
 				{
-					System.out.println("not a valid move");
+					System.out.println("not a valid box");
 				}
 			}
 			else
 			{
-				System.out.println("Player_2 enter position to fill::");
-				x = get.nextInt();
-				y = get.nextInt();
-				if(grid.validMove(x,y)==1)
+				System.out.println("Player_1 enter box number to fill::");
+				b = get.nextInt();
+				if(grid.box[b] == -1)
 				{
-					h2.fill(x,y,grid);
-					flag = 0;
+					int z = 0;
 					count++;
-
-					if(grid.winner(x,y))
+					while(z<9)
 					{
-						System.out.println("Player_2 Wins");
-						grid.showBoard();
-						break;
+						if(flag == 0)
+						{
+							System.out.println("Player_0 enter position to fill::");
+							x = get.nextInt();
+							y = get.nextInt();
+							if(grid.validMove(x,y,b)==1)
+							{
+								h1.fill(x,y,grid);
+								grid.showBoard();
+								flag = 1;
+								z++;
+								if(grid.winner(x,y))
+								{
+									System.out.println("Player_0 Wins this box");
+									grid.box[b]= 0;
+									grid.showBoard();
+									break;
+								}
+							}
+							else
+							{
+								System.out.println("not a valid move");
+							}
+						}
+						else
+						{
+							System.out.println("Player_1 enter position to fill::");
+							x = get.nextInt();
+							y = get.nextInt();
+							if(grid.validMove(x,y,b)==1)
+							{
+								h2.fill(x,y,grid);
+								grid.showBoard();
+								flag = 0;
+								z++;
+								if(grid.winner(x,y))
+								{
+									System.out.println("Player_1 Wins this box");
+									grid.box[b] = 1;
+									grid.showBoard();
+									break;
+								}
+							}
+							else
+							{
+								System.out.println("not a valid move");
+							}
+						}
 					}
 				}
 				else
 				{
-					System.out.println("not a valid move");
+					System.out.println("not a valid box");
 				}
 			}
-			grid.showBoard();
+			flag = flag ^ 1;
+			if(grid.fwinner())
+			{
+				System.out.println("player"+ flag + "wins");
+				break;
+			}
+			
 		}
 	}
 }
